417da409bb8a9207bf4c119b98cb219e37475067
Igbescobar <igbescobar@gmail.com>
2025-08-15 14:04:52 +0200

    added GNL

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -6,23 +6,32 @@
 /*   By: igngonza <igngonza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/15 13:17:04 by igngonza          #+#    #+#             */
-/*   Updated: 2025/08/15 13:39:24 by igngonza         ###   ########.fr       */
+/*   Updated: 2025/08/15 14:03:25 by igngonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# include "get_next_line.h"
 # include "libft.h"
 # include <stdio.h>
 # include <string.h>
 # include <unistd.h>
 
 // ========== Error Handling ==========
-int	exit_with_error(const char *message);
+int			exit_with_error(const char *message);
 
 // ========== Argument Parsing ==========
-int	parsing_handler(int argc, char **argv);
-int	has_cub_extension(const char *filename);
+int			parsing_handler(int argc, char **argv);
+int			has_cub_extension(const char *filename);
+
+// ========== Map Struct ==========
+
+typedef struct s_map
+{
+	char	**map;
+	int		map_height;
+}			t_map;
 
 #endif
--- includes/get_next_line.h
+++ includes/get_next_line.h
@@ -1,23 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: igngonza <igngonza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/15 12:57:19 by igngonza          #+#    #+#             */
-/*   Updated: 2025/08/15 14:04:27 by igngonza         ###   ########.fr       */
+/*   Created: 2025/08/15 14:01:38 by igngonza          #+#    #+#             */
+/*   Updated: 2025/08/15 14:01:40 by igngonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/cub3d.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-int	main(int argc, char **argv)
-{
-	int		fd;
-	t_map	map;
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10000
+# endif
 
-	if (parsing_handler(argc, argv) != 0)
-		return (1);
-	return (0);
-}
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_append(char const *s1, char const *s2);
+char	*ft_strchr(const char *string, int searchedChar);
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+
+size_t	ft_strlen(const char *s);
+
+#endif
